cfd06bc17d3aec018d2206132853f21287dffeb0
Nguyễn Bá Khoa <57384164+realkhoa@users.noreply.github.com>
2025-02-10 23:54:42 +0700

    Update run.production.sh

--- run.production.sh
+++ run.production.sh
@@ -7,8 +7,5 @@ fi
 
 docker pull realkhoa/pizza_ordering_backend:latest
 
-
-docker-compose -v | alias docker-compose="docker compose";
-
-docker-compose -f ./docker-compose.production.yml down
-docker-compose -f ./docker-compose.production.yml up -d
+docker compose -f ./docker-compose.production.yml down
+docker compose -f ./docker-compose.production.yml up -d
