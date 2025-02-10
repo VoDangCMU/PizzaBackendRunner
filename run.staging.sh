feb8cb96386f200eeedf051d4510c749effc9c82
Nguyễn Bá Khoa <57384164+realkhoa@users.noreply.github.com>
2025-02-10 23:54:53 +0700

    Update run.staging.sh

--- run.staging.sh
+++ run.staging.sh
@@ -7,7 +7,5 @@ fi
 
 docker pull realkhoa/pizza_ordering_backend_staging:latest
 
-docker-compose -v | alias docker-compose="docker compose";
-
-docker-compose -f ./docker-compose.staging.yml down
-docker-compose -f ./docker-compose.staging.yml up -d
+docker compose -f ./docker-compose.staging.yml down
+docker compose -f ./docker-compose.staging.yml up -d
